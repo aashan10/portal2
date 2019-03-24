2c6573770f6255fdb1baa6316b88d4e05a132249
Aashan Ghimire <ashanghimire10@gmail.com>
2019-03-24 12:00:14 +0545

    Votes

--- Modules/CollegeAdmin/Entities/CollegeAdmin.php
+++ Modules/CollegeAdmin/Entities/CollegeAdmin.php
@@ -3,10 +3,15 @@
 namespace Modules\CollegeAdmin\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Modules\College\Entities\College;
+use App\User;
 class CollegeAdmin extends Model
 {
     public function admin(){
-        return $this->belongsTo('App\User')->first();
+        return $this->belongsTo(User::class)->first();
+    }
+
+    public function college(){
+        return $this->belongsTo(College::class)->first();
     }
 }
--- app/Vote.php
+++ app/Vote.php
@@ -3,13 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Post;
+use App\User;
 class Vote extends Model
 {
 
-    public function posts()
-    {
-        return $this->belongsTo('App\Post');
+    public function post(){
+        return $this->belongsTo(Post::class)->first();
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class)->first();
     }
 
 
