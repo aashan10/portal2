cfb707f1ed23eeeb22e5ce54e0e3b8b39bbb0ef8
Miniyan Parajuli <parajuliminiyan@gmail.com>
2019-04-07 14:13:15 +0545

    Subjects Module

---
+++ Modules/Subjects/Routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('/admin')->name('admin.')->middleware('auth')->middleware(function($request, $next){
+    if(auth()->user()->hasRole('admin')){
+        return $next($request);
+    }
+    return view('errors.unauthorized')->with('message','You are not authorized to view this page!');
+})->group(function(){
+    Route::resource('/subject','SubjectsController');
+});
